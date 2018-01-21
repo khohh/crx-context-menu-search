65886e787c6f6d35897e4c00c70f7ee616ecc93e
Roman Grinyov <w3lifer@gmail.com>
2018-01-21 13:05:39 +0300

    1.3.0 - export/import

--- js/options.js
+++ js/options.js
@@ -5,6 +5,8 @@
 /**
  * @property chrome.contextMenus
  * @property chrome.contextMenus.removeAll
+ * @property chrome.downloads
+ * @property chrome.downloads.download
  * @property chrome.extension
  * @property chrome.extension.getBackgroundPage
  * @property chrome.storage
@@ -24,33 +26,51 @@ window.addEventListener('DOMContentLoaded', function () {
   // Initialization
 
   chrome.storage.sync.get(null, function (storage) {
-    if (storage.fieldsets) {
-        for (var index in storage.fieldsets) {
-          var fieldset = fieldsetTemplate.content.cloneNode(true);
-          if (storage.fieldsets.hasOwnProperty(index)) {
-            fieldset.querySelector('.name').value =
-              storage.fieldsets[index].name;
-            fieldset.querySelector('.url').value =
-              storage.fieldsets[index].url;
-          }
-          listOfFieldsets.insertBefore(fieldset, fieldsetTemplate);
-        }
-    }
+
+    setFieldsets(storage);
+
     if (document.querySelectorAll('.remove-fieldset-button').length === 1) {
       document.querySelector('.remove-fieldset-button').classList.add('hidden');
       document.querySelector('.up-down-arrow').classList.add('invisible');
     }
+
   });
+
+  function setFieldsets(storage) {
+
+    // Remove old fieldsets
+
+    var fieldsets = listOfFieldsets.querySelectorAll('li');
+    for (var i = 0; i < fieldsets.length; i++) {
+      fieldsets[i].remove();
+    }
+
+    // Add new fieldsets
+
+    for (var index in storage.fieldsets) {
+      var fieldset = fieldsetTemplate.content.cloneNode(true);
+      if (storage.fieldsets.hasOwnProperty(index)) {
+        fieldset.querySelector('.name').value = storage.fieldsets[index].name;
+        fieldset.querySelector('.url').value = storage.fieldsets[index].url;
+      }
+      listOfFieldsets.insertBefore(fieldset, fieldsetTemplate);
+    }
+
+  }
 
   Sortable.create(listOfFieldsets);
 
   // Events
 
+  // Events - Add fieldset
+
   addButton.addEventListener('click', function () {
+
     listOfFieldsets.insertBefore(
       fieldsetTemplate.content.cloneNode(true),
       fieldsetTemplate
     );
+
     document
       .querySelector('.remove-fieldset-button')
       .classList
@@ -59,12 +79,17 @@ window.addEventListener('DOMContentLoaded', function () {
       .querySelector('.up-down-arrow')
       .classList
       .remove('invisible');
+
   });
 
+  // Events - Save fieldsets
+
   saveButton.addEventListener('click', function () {
+
     var data = {
       fieldsets: []
     };
+
     var fieldsets = listOfFieldsets.querySelectorAll('li');
     for (var i = 0; i < fieldsets.length; i++) {
       data.fieldsets.push({
@@ -72,6 +97,7 @@ window.addEventListener('DOMContentLoaded', function () {
         url: fieldsets[i].querySelector('.url').value
       });
     }
+
     chrome.storage.sync.set(data, function () {
       savedNotification.classList.remove('hidden');
       setTimeout(function () {
@@ -81,9 +107,13 @@ window.addEventListener('DOMContentLoaded', function () {
         setContextMenuItems(data);
       });
     });
+
   });
 
+  // Events - Remove fieldset
+
   listOfFieldsets.addEventListener('click', function (eventObject) {
+
     var removeFieldSetButton = eventObject.target;
     if (removeFieldSetButton.classList.contains('remove-fieldset-button')) {
       removeFieldSetButton.parentElement.remove();
@@ -99,7 +129,61 @@ window.addEventListener('DOMContentLoaded', function () {
         firstUpDownArrow.classList.remove('invisible');
       }
     }
+
   });
+
+  // Events - Export
+
+  /**
+   * @see https://stackoverflow.com/a/23167789/4223982
+   */
+  document
+    .getElementById('export-button')
+    .addEventListener('click', function () {
+      chrome.storage.sync.get(null, function (storage) {
+        var result = JSON.stringify(storage);
+        var url = 'data:application/json;base64,' + btoa(result);
+        chrome.downloads.download({
+          url: url,
+          filename: 'chrome-extension-goto-settings.json'
+        });
+      });
+    });
+
+  // Events - Import
+
+  /**
+   * @see https://stackoverflow.com/a/36930012/4223982
+   */
+  document
+    .getElementById('import-button')
+    .addEventListener('click', function () {
+
+      var fileChooser = document.createElement('input');
+      fileChooser.type = 'file';
+
+      fileChooser.addEventListener('change', function () {
+        var file = fileChooser.files[0];
+        var reader = new FileReader();
+        reader.onload = function () {
+          var storage = JSON.parse(reader.result);
+          setFieldsets(storage);
+          saveButton.click();
+        };
+        reader.readAsText(file);
+        // Resets the input so we do get a `change` event,
+        // even if the user chooses the same file
+        form.reset();
+      });
+
+      var form = document.createElement('form');
+      form.appendChild(fileChooser);
+
+      fileChooser.click();
+
+    });
+
+  // Events - Close option window
 
   document
     .getElementById('close-button-wrapper')
