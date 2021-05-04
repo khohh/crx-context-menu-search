707225f0bb1e9c9d13193e61d8d2161d468e1666
Roman Grinyov <w3lifer@gmail.com>
2021-05-05 02:17:09 +0300

    es6 for [common.js]

--- js/common.js
+++ js/common.js
@@ -1,52 +1,49 @@
 ;'use strict';
 
-var app = {};
+const app = {};
 
 app.methods = {};
 
 app.initialStorage = {
   fieldsets: [{
     name: 'Google - Exact match',
-    url: 'https://google.com/search?q="%s"'
+    url: 'https://google.com/search?q="%s"',
   }, {
     name: 'Google Images',
-    url: 'https://google.com/search?q=%s&tbm=isch'
+    url: 'https://google.com/search?q=%s&tbm=isch',
   }, {
     name: 'YouTube',
-    url: 'https://youtube.com/results?search_query=%s'
+    url: 'https://youtube.com/results?search_query=%s',
   }, {
     name: 'Facebook',
-    url: 'https://facebook.com/search?q=%s'
+    url: 'https://facebook.com/search?q=%s',
   }, {
     name: 'Wikipedia',
-    url: 'https://wikipedia.org/wiki/%s'
+    url: 'https://wikipedia.org/wiki/%s',
   }, {
-    name: '_separator_'
+    name: '_separator_',
   }, {
     name: 'Google Maps',
-    url: 'https://google.com/maps/search/%s'
+    url: 'https://google.com/maps/search/%s',
   }, {
     name: 'Yandex Maps',
-    url: 'https://yandex.com/maps?text=%s'
-  }]
+    url: 'https://yandex.com/maps?text=%s',
+  }],
 };
 
-/**
- * @param {Array} fieldsets
- */
-app.methods.setContextMenuItems = function (fieldsets) {
-  for (var index in fieldsets) {
+app.methods.setContextMenuItems = (fieldsets) => {
+  for (const index in fieldsets) {
     if (fieldsets[index].name === '_separator_') {
       chrome.contextMenus.create({
         id: index,
         contexts: ['selection'],
-        type: 'separator'
+        type: 'separator',
       });
     } else {
       chrome.contextMenus.create({
         id: index,
         title: fieldsets[index].name,
-        contexts: ['selection']
+        contexts: ['selection'],
       });
     }
   }
